e23805400bdbce322983b94648e28e9529adc174
MohinSayyad18 <127471329+MohinSayyad18@users.noreply.github.com>
2025-05-02 11:23:57 +0530

    admin.py

--- DMS_goa/admin_web/admin.py
+++ DMS_goa/admin_web/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(DMS_State)
+admin.site.register(DMS_District)
+admin.site.register(DMS_Tahsil)
+admin.site.register(DMS_City)
+admin.site.register(DMS_Department)
+admin.site.register(DMS_Group)
+admin.site.register(DMS_Module)
+admin.site.register(DMS_SubModule)
+admin.site.register(DMS_Action)
+admin.site.register(DMS_Permission)
+admin.site.register(DMS_Employee)
+admin.site.register(DMS_WebLogin)
