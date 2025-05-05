230409ce238b93705441940fd3c80e92740c44bb
DESKTOP-50CAJ5Q\admin <waghvinayak16@gmail.com>
2025-05-05 12:11:49 +0530

    updated

--- Deployment/envsetup.sh
+++ Deployment/envsetup.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-if [ -d "vvenv" ] 
-then
+set -e  # <-- stop immediately if any command fails
+
+if [ -d "vvenv" ]; then
     echo "Python virtual environment exists." 
 else
     python3 -m venv vvenv
 fi
-echo $PWD
-(
-    source vvenv/bin/activate
-    echo "Python vvenv activated"
-    # echo "Installing current dependencies - pip install -r requirnment.txt"
-    # pip install -r requirnment.txt
-)
 
-echo "vvenv setup finishes"
+echo "Current Directory: $PWD"
+
+# Activate venv in the same shell
+source vvenv/bin/activate
+
+echo "Python vvenv activated"
+echo "Installing current dependencies - pip install -r requirements.txt"
+pip install --upgrade pip setuptools wheel   # Always upgrade pip first
+pip install -r requirements.txt
+
+echo "vvenv setup finished"
