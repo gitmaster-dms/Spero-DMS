7079da8bf9019dafd35ae2af2b2746dc74c0bf26
binduSpero488 <bindu.speroinfosystems@gmail.com>
2025-05-15 18:05:26 +0530

    Map integrated
    Map integrated

--- DMS_client/src/Componenets/DispatchModule/AlertPanel/MapView.jsx
+++ DMS_client/src/Componenets/DispatchModule/AlertPanel/MapView.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
