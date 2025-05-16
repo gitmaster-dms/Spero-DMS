c9290d038fd45d9fe849d9081dbddfaf3c2c7c1f
DESKTOP-50CAJ5Q\admin <waghvinayak16@gmail.com>
2025-05-16 11:36:13 +0530

    updated jenkins

--- Deployment/nginx.sh
+++ Deployment/nginx.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+NGINX_CONF_PATH="/var/www/html/DMS_goa/"
 if [ -f "/etc/nginx/sites-available/dms_django_9001.conf" ]; then
     sudo nginx -t
-    echo "Gunicorn Service already present"
+    echo "nginx conf already present"
 else
     echo "Copying Nginx conf dms_django_9001 file..."
-    sudo cp -rf nginx_proxy/dms_django_9001.conf /etc/nginx/sites-available/dms_django_9001.conf
+    sudo cp -rf  "${NGINX_CONF_PATH}"/Deployment/nginx_proxy/dms_django_9001.conf /etc/nginx/sites-available/dms_django_9001.conf
     chmod 777 /var/www/html/DMS_goa/
     echo "Nginx Conf copied successfully."
     sudo ln -s /etc/nginx/sites-available/dms_django_9001.conf /etc/nginx/sites-enabled
