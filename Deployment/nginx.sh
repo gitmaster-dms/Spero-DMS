4fbe27ce1f248d815c91c05db46c3faadf49bb25
DESKTOP-50CAJ5Q\admin <waghvinayak16@gmail.com>
2025-05-16 12:06:30 +0530

    created react deployment pipeline

--- Deployment/nginx.sh
+++ Deployment/nginx.sh
@@ -16,5 +16,22 @@ else
     echo "Nginx has been started"
 fi
 
+
+if [ -f "/etc/nginx/sites-available/dms_goa_react_9002.conf" ]; then
+    sudo nginx -t
+    echo "nginx conf already present"
+else
+    echo "Copying Nginx conf dms_goa_react_9002 file..."
+    sudo cp -rf  "${NGINX_CONF_PATH}"/Deployment/nginx_proxy/dms_goa_react_9002.conf /etc/nginx/sites-available/dms_goa_react_9002.conf
+    chmod 777 /var/www/html/DMS_goa/
+    echo "Nginx Conf copied successfully."
+    sudo ln -s /etc/nginx/sites-available/dms_goa_react_9002.conf /etc/nginx/sites-enabled
+    sudo nginx -t 
+    sudo systemctl start nginx
+    sudo systemctl enable nginx
+    echo "Nginx has been started"
+fi
+
+
 sudo systemctl status nginx
 sudo systemctl restart nginx
---
+++ Deployment/react_vite.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+REACT_CONF_PATH="/var/www/html/DMS_goa/DMS_client"
+
+cd "${REACT_CONF_PATH}" || { echo "React path not found: ${REACT_CONF_PATH}"; exit 1; }
+
+if [ -d "node_modules" ]; then
+    echo "node_modules exists, running npm install and build..."
+else
+    echo "node_modules does not exist, installing dependencies..."
+fi
+
+npm install
+npm run build
+
+echo "React Build Created"
