e207453e8ed92aea93868cacd72529942cdb3dbf
vinayak9775 <vinayak@sperohealthcare.in>
2025-05-20 15:59:31 +0530

    updated django setup fastapi

--- DMS_goa/DMS_fastapi_service/django_setup.py
+++ DMS_goa/DMS_fastapi_service/django_setup.py
@@ -5,12 +5,30 @@
 # django.setup()
 
 
+# import os
+# import sys
+# import django
+
+# # Add the project root to sys.path
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DMS_goa.settings")
+# django.setup()
+
+
+
 import os
 import sys
 import django
 
-# Add the project root to sys.path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# Absolute path to the directory containing DMS_goa
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Add BASE_DIR to sys.path so Python can find DMS_goa module
+sys.path.append(BASE_DIR)
+
+# Set Django settings module
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DMS_goa.settings")
+
+# Setup Django
 django.setup()
