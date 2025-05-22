37febfce7b44bee24fedc0bf219c4365943ee610
vinayak9775 <vinayak@sperohealthcare.in>
2025-05-22 16:45:26 +0530

    udpated nginx

--- Deployment/nginx.sh
+++ Deployment/nginx.sh
@@ -36,8 +36,8 @@ sudo nginx -t && {
 }
 
 # Restart FastAPI and check Django status
-sudo systemctl restart dms_uvicorn_fastapi
-sudo systemctl status dms_gunicorn_django
+# sudo systemctl restart dms_uvicorn_fastapi
+# sudo systemctl status dms_gunicorn_django
 
 # Log permissions
 echo "[~] Setting permissions on logs folder..."
@@ -45,5 +45,16 @@ sudo chown -R :dmsgroup "$LOG_PATH"
 sudo chmod -R 775 "$LOG_PATH"
 sudo chown :dmsgroup "$LOG_PATH/django_logentry.log"
 sudo chmod 664 "$LOG_PATH/django_logentry.log"
+
+
+# Permissions for log
+sudo chown www-data:www-data /var/www/html/DMS_goa/logs/django_logentry.log
+sudo chmod 664 /var/www/html/DMS_goa/logs/django_logentry.log
+sudo chown -R www-data:www-data /var/www/html/DMS_goa/logs/
+sudo chmod -R 775 /var/www/html/DMS_goa/logs/
+sudo systemctl daemon-reexec
+sudo systemctl daemon-reload
+sudo systemctl restart dms_uvicorn_fastapi
+sudo systemctl status dms_uvicorn_fastapi
 
 echo "========= NGINX Configuration Completed ========="
